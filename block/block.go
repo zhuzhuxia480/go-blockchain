2d45fa68f729a59ea5b360d7816b6860a828dc8a
zhuzhuxia <742491842@qq.com>
2024-07-22 15:00:41 +0800

    add block structure

---
+++ block/block.go
@@ -0,0 +1,33 @@
+package block
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp    int64
+	Data         []byte
+	PreBlockHash []byte
+	Hash         []byte
+}
+
+func (b *Block) SetHash() {
+	timeStamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PreBlockHash, b.Data, timeStamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, preBlockHash []byte) *Block {
+	block := &Block{
+		time.Now().Unix(),
+		[]byte(data),
+		preBlockHash,
+		[]byte{},
+	}
+	block.SetHash()
+	return block
+}
