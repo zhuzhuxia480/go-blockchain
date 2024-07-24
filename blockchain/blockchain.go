67fad570a89f7dd6dfdee046e113329e8a371737
zhuzhuxia <742491842@qq.com>
2024-07-24 21:43:44 +0800

    add itertor function in block

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -11,8 +11,32 @@ type BlockChain struct {
 	db  *bolt.DB
 }
 
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
+
 const dbFile = "blockchain.db"
 const blockBucket = "blocks"
+
+func (bc *BlockChain)Iterator() *BlockchainIterator {
+	return &BlockchainIterator{bc.tip, bc.db}
+}
+
+func (it *BlockchainIterator) Next() *Block {
+	var block *Block
+	err := it.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blockBucket))
+		obj := bucket.Get(it.currentHash)
+		block = DeSerialize(obj)
+		return nil
+	})
+	if err != nil {
+		log.Panicln(err)
+	}
+	it.currentHash = block.PreBlockHash
+	return block
+}
 
 func (bc *BlockChain) AddBlock(data string) {
 	var lastHash []byte
