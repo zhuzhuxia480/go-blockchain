598585a84a358bb2b510be7cf4e4eda8c1a486c4
zhuzhuxia <742491842@qq.com>
2024-07-24 21:37:28 +0800

    add boltDB as block storage

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,16 +1,81 @@
 package blockchain
 
+import (
+	"github.com/boltdb/bolt"
+	"log"
+	"os"
+)
+
 type BlockChain struct {
-	Blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
-func (bc *BlockChain) AddBlock(data string) {
-	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+const dbFile = "blockchain.db"
+const blockBucket = "blocks"
 
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+func (bc *BlockChain) AddBlock(data string) {
+	var lastHash []byte
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blockBucket))
+		lastHash = bucket.Get([]byte("l"))
+		return nil
+	})
+	if err != nil {
+		log.Panicln("err:", err)
+	}
+	block := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blockBucket))
+		err := bucket.Put([]byte("l"), block.Hash)
+		if err != nil {
+			return err
+		}
+		err = bucket.Put(block.Hash, block.Serialize())
+		if err != nil {
+			return err
+		}
+		bc.tip = block.Hash
+		return nil
+	})
+	if err != nil {
+		log.Panicln(err)
+	}
+
 }
 
 func NewBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open(dbFile, os.ModePerm, nil)
+	if err != nil {
+		log.Panicln("err:", err)
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blockBucket))
+		if b == nil {
+			log.Println("this is no exist blockchain in db, create a new one")
+			b, err = b.CreateBucket([]byte(blockBucket))
+			if err != nil {
+				return err
+			}
+			block := NewGenesisBlock()
+			err := b.Put(block.Hash, block.Serialize())
+			if err != nil {
+				return err
+			}
+			err = b.Put([]byte("l"), block.Hash)
+			if err != nil {
+				return err
+			}
+			tip = block.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panicln("db update err:", err)
+	}
+	return &BlockChain{tip, db}
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module go-blockchain
+
+go 1.21.3
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+)
