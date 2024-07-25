42a830019b3f49a4132d03e577e47e78d0932815
zhuzhuxia <742491842@qq.com>
2024-07-25 09:29:29 +0800

    add cli interface

---
+++ blockchain/cli.go
@@ -0,0 +1,118 @@
+package blockchain
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strconv"
+)
+
+type CLI struct {
+	bc *BlockChain
+}
+
+func (cli *CLI) printUsage() {
+	log.Println("Usage:")
+	log.Println("addblock --data BLOCK_DATA - add a block to the blockchain")
+	log.Println("printchain - print all the blocks  of the blockchain")
+}
+
+func (cli *CLI) validArgs() {
+	if len(os.Args) < 2{
+		cli.printUsage()
+		os.Exit(1)
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+}
+
+func (cli *CLI) printChain() {
+	iterator := cli.bc.Iterator()
+	for  {
+		block := iterator.Next()
+		log.Printf("Pre hash: %x\n", block.PreBlockHash)
+		log.Println("Data: ", block.Data)
+		log.Println("Hash: ", block.Hash)
+		pow := NewProofOfWork(block)
+		log.Println("Pow: ", strconv.FormatBool(pow.Validate()))
+		if len(block.PreBlockHash) == 0 {
+			log.Println("iterator chain completed")
+			break
+		}
+	}
+}
+
+func (cli *CLI) Run() {
+	cli.validArgs()
+	addblockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	addblockData := addblockCmd.String("data", "", "block data")
+	switch os.Args[1] {
+	case "addblock":
+		err := addblockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panicln(err)
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panicln(err)
+		}
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+	if addblockCmd.Parsed() {
+		if *addblockData == "" {
+			addblockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addblockData)
+	}
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
