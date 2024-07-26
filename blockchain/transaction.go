2e2ff9b6e9387edd2a266b0b9e8cb652e8d7d9ff
zhuzhuxia <742491842@qq.com>
2024-07-26 16:20:39 +0800

    add transaction.go

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -4,8 +4,12 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/gob"
+	"encoding/hex"
+	"fmt"
 	"log"
 )
+
+const subsidy = 10
 
 type Transaction struct {
 	ID   []byte
@@ -29,6 +33,7 @@ func (tx Transaction) IsCoinbase() bool {
 	return len(tx.Vin) == 1 && len(tx.Vin[0].Txid) == 0 && tx.Vin[0].Vout == -1
 }
 
+// SetID sets ID of a transaction
 func (tx *Transaction) SetID() {
 	var encodBuf bytes.Buffer
 	encoder := gob.NewEncoder(&encodBuf)
@@ -38,4 +43,67 @@ func (tx *Transaction) SetID() {
 	}
 	hash := sha256.Sum256(encodBuf.Bytes())
 	tx.ID = hash[:]
+}
+
+// CanUnlockOutputWith checks whether the address initiated the transaction
+func (in *TXInput) CanUnlockOutputWith(unlockingData string) bool {
+	return in.ScriptSig == unlockingData
+}
+
+// CanBeUnlockedWith checks if the output can be unlocked with the provided data
+func (out *TXOutput) CanBeUnlockedWith(unlockingData string) bool {
+	return out.ScriptPubKey == unlockingData
+}
+
+// NewCoinbaseTX creates a new coinbase transaction
+func NewCoinbaseTX(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+	txin := TXInput{[]byte{}, -1, data}
+	txout := TXOutput{subsidy, to}
+	tx := Transaction{
+		nil,
+		[]TXInput{txin},
+		[]TXOutput{txout},
+	}
+	tx.SetID()
+	return &tx
+}
+
+// NewUTXOTransaction create a new transaction
+func NewUTXOTransaction(from, to string, amount int, bc *BlockChain) *Transaction {
+	var inputs []TXInput
+	var outputs []TXOutput
+
+	acc, validOutputs := bc.FindSpendableOutputs(from, amount)
+	if acc < amount {
+		log.Panicln("ERROR: Not enough funds")
+	}
+
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
+		if err != nil {
+			log.Panicln(err)
+		}
+		for _, out := range outs {
+			input := TXInput{
+				Txid:      txID,
+				Vout:      out,
+				ScriptSig: from,
+			}
+			inputs = append(inputs, input)
+		}
+	}
+	outputs = append(outputs, TXOutput{amount, to})
+	if acc > amount {
+		outputs = append(outputs, TXOutput{acc - amount, to})
+	}
+	tx := Transaction{
+		ID:   nil,
+		Vin:  inputs,
+		Vout: outputs,
+	}
+	tx.SetID()
+	return &tx
 }
