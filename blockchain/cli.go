d98efde41c46d8013f87890828cdab07d3a77170
zhuzhuxia <742491842@qq.com>
2024-07-26 16:22:56 +0800

    add createBlockchain in cli

--- blockchain/cli.go
+++ blockchain/cli.go
@@ -8,32 +8,50 @@ import (
 )
 
 type CLI struct {
-	Bc *BlockChain
+}
+
+func (cli *CLI) createBlockchain(address string) {
+	blockchain := CreateBlockchain(address)
+	blockchain.Db.Close()
+	log.Println("Create BlockChain Done!")
+}
+
+func (cli *CLI) getBalance(address string) {
+	bc := NewBlockChain(address)
+	defer bc.Db.Close()
+
+	balance := 0
+	UTXOs := bc.FindUTXO(address)
+
+	for _, out := range UTXOs {
+		balance += out.Value
+	}
+	log.Printf("Balance of '%s' is : '%d'", address, balance)
 }
 
 func (cli *CLI) printUsage() {
 	log.Println("Usage:")
-	log.Println("addblock --data BLOCK_DATA - add a block to the blockchain")
-	log.Println("printchain - print all the blocks  of the blockchain")
+	log.Println("  getbalance --address ADDRESS - Get balance of ADDRESS")
+	log.Println("  createblockchain --address ADDRESS - Create a blockchain and send genesis block reward to ADDRESS")
+	log.Println("  printchain - Print all the blocks  of the blockchain")
+	log.Println("  send -from FROM -to TO -amount AMOUNT - Send AMOUNT of coins from FROM to TO")
 }
 
 func (cli *CLI) validArgs() {
-	if len(os.Args) < 2{
+	if len(os.Args) < 2 {
 		cli.printUsage()
 		os.Exit(1)
 	}
 }
 
-func (cli *CLI) addBlock(data string) {
-	cli.Bc.AddBlock(data)
-}
 
 func (cli *CLI) printChain() {
-	iterator := cli.Bc.Iterator()
-	for  {
+	bc := NewBlockChain("")
+	defer bc.Db.Close()
+	iterator := bc.Iterator()
+	for {
 		block := iterator.Next()
 		log.Printf("Pre hash: %x\n", block.PreBlockHash)
-		log.Println("Data: ", string(block.Data))
 		log.Printf("Hash: %x\n", block.Hash)
 		pow := NewProofOfWork(block)
 		log.Println("Pow: ", strconv.FormatBool(pow.Validate()))
@@ -43,6 +61,15 @@ func (cli *CLI) printChain() {
 			break
 		}
 	}
+}
+
+func (cli *CLI) send(from, to string, amount int) {
+	bc := NewBlockChain(from)
+	defer bc.Db.Close()
+
+	tx := NewUTXOTransaction(from, to, amount, bc)
+	bc.MineBLock([]*Transaction{tx})
+	log.Println("Success!")
 }
 
 func (cli *CLI) Run() {
@@ -76,44 +103,3 @@ func (cli *CLI) Run() {
 		cli.printChain()
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
