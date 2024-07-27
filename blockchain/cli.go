9a23d74f84334fe78d3f49f622a8ccb0f6784739
zhuzhuxia <742491842@qq.com>
2024-07-27 12:12:49 +0800

    add arg parse in cli.go

--- blockchain/cli.go
+++ blockchain/cli.go
@@ -29,14 +29,6 @@ func (cli *CLI) getBalance(address string) {
 	log.Printf("Balance of '%s' is : '%d'", address, balance)
 }
 
-func (cli *CLI) printUsage() {
-	log.Println("Usage:")
-	log.Println("  getbalance --address ADDRESS - Get balance of ADDRESS")
-	log.Println("  createblockchain --address ADDRESS - Create a blockchain and send genesis block reward to ADDRESS")
-	log.Println("  printchain - Print all the blocks  of the blockchain")
-	log.Println("  send -from FROM -to TO -amount AMOUNT - Send AMOUNT of coins from FROM to TO")
-}
-
 func (cli *CLI) validArgs() {
 	if len(os.Args) < 2 {
 		cli.printUsage()
@@ -72,14 +64,35 @@ func (cli *CLI) send(from, to string, amount int) {
 	log.Println("Success!")
 }
 
+func (cli *CLI) printUsage() {
+	log.Println("Usage:")
+	log.Println("  getbalance --address ADDRESS - Get balance of ADDRESS")
+	log.Println("  createblockchain --address ADDRESS - Create a blockchain and send genesis block reward to ADDRESS")
+	log.Println("  printchain - Print all the blocks  of the blockchain")
+	log.Println("  send -from FROM -to TO -amount AMOUNT - Send AMOUNT of coins from FROM to TO")
+}
+
 func (cli *CLI) Run() {
 	cli.validArgs()
-	addblockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	getBalanceCmd := flag.NewFlagSet("getbalance", flag.ExitOnError)
+	createBlockchainCmd := flag.NewFlagSet("createblockchain", flag.ExitOnError)
+	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
 	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
-	addblockData := addblockCmd.String("data", "", "block data")
+
+	getBalanceAddress := getBalanceCmd.String("address", "", "the address to get balance for")
+	createBlockchainAddress := createBlockchainCmd.String("address", "", "the address to send genesis reward to")
+	sendTo := sendCmd.String("to", "", "destination wallet address")
+	sendFrom := sendCmd.String("from", "", "source wallet address")
+	sendAmount := sendCmd.Int("amount", 0, "Amount to send")
+
 	switch os.Args[1] {
-	case "addblock":
-		err := addblockCmd.Parse(os.Args[2:])
+	case "getbalance":
+		err := getBalanceCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panicln(err)
+		}
+	case "createblockchain":
+		err := createBlockchainCmd.Parse(os.Args[2:])
 		if err != nil {
 			log.Panicln(err)
 		}
@@ -88,18 +101,41 @@ func (cli *CLI) Run() {
 		if err != nil {
 			log.Panicln(err)
 		}
+	case "send":
+		err := sendCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panicln(err)
+		}
 	default:
 		cli.printUsage()
 		os.Exit(1)
 	}
-	if addblockCmd.Parsed() {
-		if *addblockData == "" {
-			addblockCmd.Usage()
+
+	if getBalanceCmd.Parsed() {
+		if *getBalanceAddress == "" {
+			getBalanceCmd.Usage()
 			os.Exit(1)
 		}
-		cli.addBlock(*addblockData)
+		cli.getBalance(*getBalanceAddress)
 	}
+
+	if createBlockchainCmd.Parsed() {
+		if *createBlockchainAddress == "" {
+			createBlockchainCmd.Usage()
+			os.Exit(1)
+		}
+		cli.createBlockchain(*createBlockchainAddress)
+	}
+
 	if printChainCmd.Parsed() {
 		cli.printChain()
+	}
+
+	if sendCmd.Parsed() {
+		if *sendTo == "" || *sendFrom == "" || *sendAmount <= 0 {
+			sendCmd.Usage()
+			os.Exit(1)
+		}
+		cli.send(*sendFrom, *sendTo, *sendAmount)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module go-blockchain
 
 go 1.21.3
 
-require (
-	github.com/boltdb/bolt v1.3.1 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-)
+require github.com/boltdb/bolt v1.3.1
+
+require golang.org/x/sys v0.22.0 // indirect
