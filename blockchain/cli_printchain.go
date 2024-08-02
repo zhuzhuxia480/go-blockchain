8ec64488810e5f66b882954d2bc89b9940d82857
zhuzhuxia <742491842@qq.com>
2024-08-02 17:03:18 +0800

    fix printChain

--- blockchain/cli_printchain.go
+++ blockchain/cli_printchain.go
@@ -5,13 +5,14 @@ import (
 	"strconv"
 )
 
-func (cli *CLI) printChain() {
-	bc := NewBlockChain()
+func (cli *CLI) printChain(nodeID string) {
+	bc := NewBlockChain(nodeID)
 	defer bc.Db.Close()
 	iterator := bc.Iterator()
 	for {
 		block := iterator.Next()
 		log.Printf("============= Block %x =============\n", block.Hash)
+		log.Printf("Height: %x\n", block.Height)
 		log.Printf("Prev. block.hash: %x\n", block.PreBlockHash)
 		pow := NewProofOfWork(block)
 		log.Println("Pow: ", strconv.FormatBool(pow.Validate()))
