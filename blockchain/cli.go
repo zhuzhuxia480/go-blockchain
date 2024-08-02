9ccf5aee8205eab8fcaa28ec1d89a3aa6b1d2f4a
zhuzhuxia <742491842@qq.com>
2024-08-02 18:39:34 +0800

    fix cli

--- blockchain/cli.go
+++ blockchain/cli.go
@@ -24,22 +24,33 @@ func (cli *CLI) printUsage() {
 	log.Println("  listaddresses - Lists all addresses from the wallet file")
 	log.Println("  printchain - Print all the blocks of the blockchain")
 	log.Println("  send -from FROM -to TO -amount AMOUNT - Send AMOUNT of coins from FROM address to TO")
+	log.Println("  send -from FROM -to TO -amount AMOUNT -mine - Send AMOUNT of coins from FROM address to TO")
+	log.Println("  startnode -miner ADDRESS - Start a node with ID specified in NODE_ID env. -miner enables mining")
 }
 
 func (cli *CLI) Run() {
 	cli.validArgs()
+
+	nodeID := os.Getenv("NODE_ID")
+	if nodeID == "" {
+		log.Panicln("NODE_ID env is not set")
+	}
+
 	getBalanceCmd := flag.NewFlagSet("getbalance", flag.ExitOnError)
 	createBlockchainCmd := flag.NewFlagSet("createblockchain", flag.ExitOnError)
 	createWalletCmd := flag.NewFlagSet("createwallet", flag.ExitOnError)
 	listAddressesCmd := flag.NewFlagSet("listaddresses", flag.ExitOnError)
 	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
 	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	startNodeCmd := flag.NewFlagSet("startnode", flag.ExitOnError)
 
 	getBalanceAddress := getBalanceCmd.String("address", "", "the address to get balance for")
 	createBlockchainAddress := createBlockchainCmd.String("address", "", "the address to send genesis reward to")
 	sendTo := sendCmd.String("to", "", "destination wallet address")
 	sendFrom := sendCmd.String("from", "", "source wallet address")
 	sendAmount := sendCmd.Int("amount", 0, "Amount to send")
+	sendMine := startNodeCmd.Bool("mine", false, "Mine immediately on the same node")
+	startNodeMiner := startNodeCmd.String("miner", "", "")
 
 	switch os.Args[1] {
 	case "getbalance":
