9bdebacd4b51aa9288f619b24f05b26c32f09226
zhuzhuxia <742491842@qq.com>
2024-08-02 17:00:19 +0800

    fix createwallet

--- blockchain/cli_createwallet.go
+++ blockchain/cli_createwallet.go
@@ -2,9 +2,9 @@ package blockchain
 
 import "log"
 
-func (cli *CLI) createWallet() {
-	wallets, _ := NewWallets()
+func (cli *CLI) createWallet(nodeID string) {
+	wallets, _ := NewWallets(nodeID)
 	address := wallets.CreateWallet()
-	wallets.SaveToFile()
+	wallets.SaveToFile(nodeID)
 	log.Printf("You new address: %s\n", address)
 }
