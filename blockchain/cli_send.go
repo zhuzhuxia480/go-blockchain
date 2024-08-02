4e8f1cf607aa8e8d6a3db984cd965629219de024
zhuzhuxia <742491842@qq.com>
2024-08-02 18:18:53 +0800

    fix send

--- blockchain/cli_send.go
+++ blockchain/cli_send.go
@@ -2,7 +2,7 @@ package blockchain
 
 import "log"
 
-func (cli *CLI) send(from, to string, amount int) {
+func (cli *CLI) send(from, to string, amount int, nodeID string, mineNow bool) {
 	if !ValidateAddress(from) {
 		log.Panicln("ERROR: sender address is not valid")
 	}
@@ -10,13 +10,23 @@ func (cli *CLI) send(from, to string, amount int) {
 		log.Panicln("ERROR: recipient address is valid")
 	}
 
-	bc := NewBlockChain()
+	bc := NewBlockChain(nodeID)
 	UTXOSet := UTXOSet{bc}
 	defer bc.Db.Close()
 
-	tx := NewUTXOTransaction(from, to, amount, &UTXOSet)
-	coinbaseTX := NewCoinbaseTX(from, "")
-	newBlock := bc.MineBLock([]*Transaction{tx, coinbaseTX})
-	UTXOSet.Update(newBlock)
+	wallets, err := NewWallets(nodeID)
+	if err != nil {
+		log.Panicln(err)
+	}
+	wallet := wallets.GetWallet(from)
+	tx := NewUTXOTransaction(&wallet, to, amount, &UTXOSet)
+
+	if mineNow {
+		coinbaseTX := NewCoinbaseTX(from, "")
+		newBlock := bc.MineBLock([]*Transaction{tx, coinbaseTX})
+		UTXOSet.Update(newBlock)
+	} else {
+		sendTx(knownNodes[0], tx)
+	}
 	log.Println("Success!")
 }
